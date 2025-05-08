6ae6dbe695b147886923d0718517bfe3a31beadd
Nyasha Musanhu <danielsnyasha@outlook.com>
2025-05-08 20:19:14 +0200

    06: categories final

--- src/components/search-filters/subcategory-menu.tsx
+++ src/components/search-filters/subcategory-menu.tsx
@@ -1,9 +1,10 @@
+import { CustomCategory } from '@/app/(home)/types';
 import { Category } from '@/payload-types';
 import Link from 'next/link';
 import React from 'react'
 
 type Props = {
-    category:Category;
+    category:CustomCategory;
     isOpen: boolean;
     position: {top:number; left: number}
 }
@@ -25,7 +26,7 @@ export default function SubcategoryMenu({category, isOpen,position}: Props) {
         <div className='h-3 w-60'/>
         <div style={{backgroundColor}} className='w-60 text-black rounded-md overflow-hidden border shadow-[4px_4px_0px_0px_rgba(0,0,0,1)] -translate-x-[2px] -translate-y-[2px]'>
             {category.subcategories?.map((subcategory:Category)=>(
-                <Link key={subcategory.slug} href="/" className='w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center hover:underline font-medium'>
+                <Link key={subcategory.slug} href={`/${category.slug}/${subcategory.slug}`} className='w-full text-left p-4 hover:bg-black hover:text-white flex justify-between items-center hover:underline font-medium'>
                     {subcategory.name}
                 </Link>
             ))}
